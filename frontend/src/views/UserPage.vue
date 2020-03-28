d97c2ff732c91a754cb8de8f466732623c06be74
baleian <baleian90@gmail.com>
2020-03-29 01:53:04 +0900

    installed apexchart and maked templete of donut chart

--- frontend/src/views/UserPage.vue
+++ frontend/src/views/UserPage.vue
@@ -6,6 +6,11 @@
         <span>{{ userId }}</span>
       </v-col>
     </v-row>
+    <v-row justify="center">
+      <v-col align="center" cols="6">
+        <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -13,8 +18,54 @@
 import { mapState, mapMutations } from 'vuex';
 import { SET_USER_ID } from '@/store/modules/user';
 
+function toMoneyString(value) {
+  let vs = (''+value).split('').reverse();
+  for (var i = 0; i < vs.length - 1; i++) {
+    if (i % 3 === 2) {
+      vs[i] = ',' + vs[i];
+    }
+  }
+  return vs.reverse().join('') + '\uFFE6';
+}
+
 export default {
   name: 'UserPage',
+
+  data: () => ({
+    series: [ 316060, 93870, 38000, 17600, 700, 44200 ],
+    chartOptions: {
+      labels: ['온라인쇼핑', '편의점', '마트', '커피와 디저트', '생활', '카테고리 미지정'],
+      legend: {
+        position: 'top',
+      },
+      chart: { type: 'donut', },
+      dataLabels: {
+        dropShadow: { enabled: true, },
+      },
+      tooltip: {
+        y: { formatter: toMoneyString }
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              value: {
+                formatter: toMoneyString,
+              },
+              total: {
+                show: true,
+                formatter: (v) => {
+                  let total = v.globals.series.reduce((a, b) => a + b);
+                  return toMoneyString(total);
+                },
+              }, 
+            },
+          }
+        }
+      }
+    },
+  }),
 
   computed: {
     ...mapState('user', ['userId']),
@@ -22,7 +73,6 @@ export default {
 
   methods: {
     ...mapMutations('user', { SET_USER_ID }),
-
     initPage(userId) {
       this.SET_USER_ID(userId);
     },
